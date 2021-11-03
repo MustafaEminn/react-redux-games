722ed68eb5cea076b1e482c6a0d3989dcf90b44b
Mustafa Emin Bosnalı <mustafaeminbosnali@hotmail.com>
2021-11-04 00:26:22 +0300

    index.tsx; Router and ThemeButton added.

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM from "react-dom";
+import "./index.scss";
+import HomeView from "./views/Home/Home";
+import { store } from "./store/store";
+import { Provider } from "react-redux";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import ToggleThemeButton from "./components/input/button/toggleTheme/ToggleThemeButton";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/">
+          <HomeView />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+    <ToggleThemeButton />
+  </Provider>,
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
