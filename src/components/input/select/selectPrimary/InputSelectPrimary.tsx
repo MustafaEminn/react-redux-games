62eabaf2ce27645cc9bcb85c63e586256e80a9dc
Mustafa Emin Bosnalı <mustafaeminbosnali@hotmail.com>
2021-11-04 16:06:35 +0300

    InputSelectPrimary; InputList added. Focus and blur functions added. visible state added.

--- src/components/input/select/selectPrimary/InputSelectPrimary.tsx
+++ src/components/input/select/selectPrimary/InputSelectPrimary.tsx
@@ -1,28 +1,56 @@
+import { useState } from "react";
 import ArrowDownIcon from "../../../icons/ArrowDownIcon";
+import InputList from "../../list/InputList";
 import s from "./InputSelectPrimary.module.scss";
 
 interface IInputSelectPrimary {
   placeholder?: string | undefined;
   prefixIcon?: React.ReactNode;
   multipleSelect?: boolean;
+  list: string[];
 }
 
 const InputSelectPrimary = ({
   placeholder,
   prefixIcon,
   multipleSelect = false,
+  list,
 }: IInputSelectPrimary) => {
+  const [visible, setVisible] = useState<boolean>(false);
+
+  const onInputFocus = (): void => {
+    setVisible(true);
+  };
+
+  const onInputBlur = (): void => {
+    setVisible(false);
+  };
+
   return (
-    <div className={s.inputContainer}>
-      <input className={s.input} placeholder={placeholder} />
-      {prefixIcon ? (
-        <div className={s.inputPrefixIcon}>{prefixIcon}</div>
-      ) : (
-        <></>
-      )}
-      <div className={s.inputSuffixIcon}>
-        <ArrowDownIcon />
+    <div className={s.container}>
+      <div className={s.inputContainer}>
+        <input
+          onFocus={() => onInputFocus()}
+          onBlur={() => onInputBlur()}
+          className={s.input}
+          placeholder={placeholder}
+          type="text"
+        />
+        {prefixIcon ? (
+          <div className={s.inputPrefixIcon}>{prefixIcon}</div>
+        ) : (
+          <></>
+        )}
+        <div className={s.inputSuffixIcon}>
+          <ArrowDownIcon />
+        </div>
       </div>
+
+      <InputList
+        multipleSelect={multipleSelect}
+        list={list}
+        visible={visible}
+      />
     </div>
   );
 };
