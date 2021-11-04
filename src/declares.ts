72bd23096760615ae618ec093fc2ea2f0f6aaba2
Mustafa Emin Bosnalı <mustafaeminbosnali@hotmail.com>
2021-11-04 16:06:48 +0300

    declares.ts; Added

---
+++ src/declares.ts
@@ -0,0 +1,9 @@
+import React from "react";
+
+declare module "react" {
+  interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
+    // extends React's HTMLAttributes
+    visible?: string;
+    isSelected?: string;
+  }
+}
