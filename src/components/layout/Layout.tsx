c3c242bfda3c20e5b47f892ae4813bfbbb21239e
Mustafa Emin Bosnalı <mustafaeminbosnali@hotmail.com>
2021-11-07 14:35:18 +0300

    Layout.tsx; Loading prop added.

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,12 +1,16 @@
+import LoadingView from "../loading/LoadingView";
 import Navbar from "../navbar/Navbar";
 import s from "./Layout.module.scss";
 
 interface ILayout {
   children?: React.ReactNode;
+  loading?: boolean;
 }
 
-function Layout({ children }: ILayout) {
-  return (
+function Layout({ children, loading = false }: ILayout) {
+  return loading ? (
+    <LoadingView />
+  ) : (
     <div className={s.container}>
       <Navbar />
       {children}
