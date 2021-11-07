fe4ff43aa0abe467bbef9c1e2e47524ceccad915
Mustafa Emin Bosnalı <mustafaeminbosnali@hotmail.com>
2021-11-07 14:34:29 +0300

    Logo.tsx; Link option added.

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -2,11 +2,17 @@ import { Link } from "react-router-dom";
 import { BASE } from "../../config/base";
 import s from "./Logo.module.scss";
 
-function Logo() {
-  return (
+interface ILogo {
+  link?: boolean;
+}
+
+function Logo({ link = true }: ILogo) {
+  return link ? (
     <Link to="/" className={s.logo}>
       {BASE.APP_NAME}
     </Link>
+  ) : (
+    <h1 className={s.logo}>{BASE.APP_NAME}</h1>
   );
 }
 
