3dcfdabcd86001788b0e31088e2bd06647c6c165
Mustafa Emin Bosnalı <mustafaeminbosnali@hotmail.com>
2021-11-07 20:43:40 +0300

    Navbar.tsx; Page links added.

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,13 +1,34 @@
+import { Col, Row } from "antd";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { setSidebarVisibleValue } from "../../store/sidebar";
+import BarsIcon from "../icons/BarsIcon";
 import Logo from "../logo/Logo";
 import s from "./Navbar.module.scss";
 
 const Navbar = () => {
+  const dispatch = useDispatch();
+
+  const openSidebar = () => {
+    dispatch(setSidebarVisibleValue(true));
+  };
   return (
-    <div className={s.container}>
-      <div className={s.containerLeft}>
+    <Row wrap={false} className={s.container}>
+      <Col className={s.containerLeft}>
         <Logo />
-      </div>
-    </div>
+      </Col>
+      <Col className={s.containerRight}>
+        <Link className={s.containerRight_link} to="/">
+          Home
+        </Link>
+        <Link className={s.containerRight_link} to="/favorites">
+          Favorites
+        </Link>
+        <button onClick={() => openSidebar()} className={s.containerRight_bars}>
+          <BarsIcon />
+        </button>
+      </Col>
+    </Row>
   );
 };
 
