7966d7e133c5fc1abc14d5abb7859be472b98ed2
Mustafa Emin Bosnalı <mustafaeminbosnali@hotmail.com>
2021-11-07 14:32:16 +0300

    axios.ts; Added.

---
+++ src/axios.ts
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+axios.defaults.headers.common["x-rapidapi-host"] =
+  "free-to-play-games-database.p.rapidapi.com";
+axios.defaults.headers.common["x-rapidapi-key"] =
+  "7684488581msh818f5cea6277451p117a62jsnbc9105f92f98";
