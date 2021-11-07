19a3c2c185dbda8233de025bff3a05a406de49fb
Mustafa Emin Bosnalı <mustafaeminbosnali@hotmail.com>
2021-11-07 14:33:16 +0300

    index.tsx; ToastContainer and axios path added.

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import ReactDOM from "react-dom";
-import "./index.scss";
 import HomeView from "./views/Home/Home";
 import { store } from "./store/store";
 import { Provider } from "react-redux";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "./axios";
+import "react-toastify/dist/ReactToastify.css";
 
 import "./declares";
 import "antd/dist/antd.css";
+import "./index.scss";
+import { ToastContainer } from "react-toastify";
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
@@ -16,6 +19,17 @@ ReactDOM.render(
         </Route>
       </Switch>
     </BrowserRouter>
+    <ToastContainer
+      position="bottom-right"
+      autoClose={2500}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+    />
   </Provider>,
   document.getElementById("root")
 );
