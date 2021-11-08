99094e8f4db6ab37b2271b1d9a6e9c7880d820df
Mustafa Emin Bosnalı <mustafaeminbosnali@hotmail.com>
2021-11-08 22:28:24 +0300

    Game.tsx; Types imported from types folder.

--- src/views/Game/Game.tsx
+++ src/views/Game/Game.tsx
@@ -9,37 +9,7 @@ import Carousel, { arrowsPlugin, Dots } from "@brainhubeu/react-carousel";
 import "@brainhubeu/react-carousel/lib/style.css";
 import s from "./Game.module.scss";
 import { LeftOutlined, RightOutlined } from "@ant-design/icons";
-
-interface IDatasMinimumSystemReq {
-  os: string;
-  processor: string;
-  memory: string;
-  graphics: string;
-  storage: string;
-}
-
-interface IDatasScreenshots {
-  id: number;
-  image: string;
-}
-
-interface IDatas {
-  id: number;
-  title: string;
-  thumbnail: string;
-  status: string;
-  short_description: string;
-  description: string;
-  game_url: string;
-  genre: string;
-  platform: string;
-  publisher: string;
-  developer: string;
-  release_date: string;
-  freetogame_profile_url: string;
-  minimum_system_requirements: IDatasMinimumSystemReq;
-  screenshots: IDatasScreenshots;
-}
+import { IDatas, IDatasScreenshots } from "../../types/datas";
 
 const GameView = () => {
   const [pageLoading, setPageLoading] = useState<boolean>(true);
