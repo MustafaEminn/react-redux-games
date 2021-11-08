05918a9d3701dfe1eb35c1fcaebc72f4c3bebfa7
Mustafa Emin Bosnalı <mustafaeminbosnali@hotmail.com>
2021-11-08 22:28:02 +0300

    Favorites.tsx; Types imported from types folder. loading state removed.

--- src/views/Favorites/Favorites.tsx
+++ src/views/Favorites/Favorites.tsx
@@ -1,11 +1,7 @@
 import { useEffect, useState } from "react";
 
-import { Row, Col, Card, Spin, Button, Empty, Space } from "antd";
-import {
-  ArrowRightOutlined,
-  HeartFilled,
-  LoadingOutlined,
-} from "@ant-design/icons";
+import { Row, Col, Card, Button, Empty } from "antd";
+import { ArrowRightOutlined, HeartFilled } from "@ant-design/icons";
 
 import Layout from "../../components/layout/Layout";
 import s from "./Favorites.module.scss";
@@ -14,44 +10,8 @@ import { BASE } from "../../config/base";
 import { toast } from "react-toastify";
 import { Link } from "react-router-dom";
 import slug from "slug";
-
-interface IDatasMinimumSystemReq {
-  os: string;
-  processor: string;
-  memory: string;
-  graphics: string;
-  storage: string;
-}
-
-interface IDatasScreenshots {
-  id: number;
-  image: string;
-}
-
-interface IDatas {
-  id: number;
-  title: string;
-  thumbnail: string;
-  status: string;
-  short_description: string;
-  description: string;
-  game_url: string;
-  genre: string;
-  platform: string;
-  publisher: string;
-  developer: string;
-  release_date: string;
-  freetogame_profile_url: string;
-  addedAt: string;
-  minimum_system_requirements: IDatasMinimumSystemReq;
-  screenshots: IDatasScreenshots;
-}
-
-interface IFavorites {
-  gameId: number;
-  // Please use new Date().toLocaleDateString()
-  addedAt: string;
-}
+import { IDatas } from "../../types/datas";
+import { IFavorites } from "../../types/favorites";
 
 function FavoritesView() {
   const [datas, setDatas] = useState<Array<IDatas>>([]);
@@ -59,7 +19,6 @@ function FavoritesView() {
     (localStorage.favorites && JSON.parse(localStorage.favorites)) || []
   );
   const [pageLoading, setPageLoading] = useState<boolean>(true);
-  const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
     if (!localStorage.favorites) {
@@ -117,90 +76,72 @@ function FavoritesView() {
 
   return (
     <Layout loading={pageLoading}>
-      {loading ? (
-        <Row justify="center">
-          <Spin
-            indicator={
-              <LoadingOutlined style={{ fontSize: 40, color: "#000" }} spin />
-            }
-          />
-        </Row>
-      ) : (
-        <Row
-          gutter={[16, 16]}
-          className={s.cardsContainer}
-          justify="start"
-          wrap
-        >
-          {favorites.length === 0 && (
-            <Empty
-              className={s.empty}
-              description="You has not favorite game."
-            />
-          )}
-          {datas &&
-            datas.map((item: IDatas, index: number) => (
-              <Col
-                key={index}
-                xs={24}
-                sm={24}
-                md={8}
-                xl={6}
-                className={s.cardContainer}
-              >
-                <Card bodyStyle={{ padding: 0 }}>
-                  <Link to={`/${slug(item.title)}/${item.id}`}>
-                    <img loading="lazy" src={item.thumbnail} />
-                  </Link>
-                  <Link to={`/${slug(item.title)}/${item.id}`}>
-                    <h1 className={s.cardContainer_title}>{item.title}</h1>
-                  </Link>
+      <Row gutter={[16, 16]} className={s.cardsContainer} justify="start" wrap>
+        {favorites.length === 0 && (
+          <Empty className={s.empty} description="You has not favorite game." />
+        )}
+        {datas &&
+          datas.map((item: IDatas, index: number) => (
+            <Col
+              key={index}
+              xs={24}
+              sm={24}
+              md={8}
+              xl={6}
+              className={s.cardContainer}
+            >
+              <Card bodyStyle={{ padding: 0 }}>
+                <Link to={`/${slug(item.title)}/${item.id}`}>
+                  <img loading="lazy" src={item.thumbnail} />
+                </Link>
+                <Link to={`/${slug(item.title)}/${item.id}`}>
+                  <h1 className={s.cardContainer_title}>{item.title}</h1>
+                </Link>
+                <p className={s.cardContainer_shortDescription}>
+                  {item.short_description}
+                </p>
+
+                <div className={s.cardContainer_genre}>
+                  <h3>Genre:</h3>
                   <p className={s.cardContainer_shortDescription}>
-                    {item.short_description}
+                    {item.genre}
                   </p>
+                </div>
+                <div className={s.cardContainer_addedAt}>
+                  <h3>Added at:</h3>
+                  <p>{item.addedAt}</p>
+                </div>
 
-                  <div className={s.cardContainer_genre}>
-                    <h3>Genre:</h3>
-                    <p className={s.cardContainer_shortDescription}>
-                      {item.genre}
-                    </p>
-                  </div>
-                  <div className={s.cardContainer_addedAt}>
-                    <h3>Added at:</h3>
-                    <p>{item.addedAt}</p>
-                  </div>
-
-                  <Row className={s.cardContainer_buttons}>
-                    <Col md={12} sm={24} xs={24}>
+                <Row className={s.cardContainer_buttons}>
+                  <Col md={12} sm={24} xs={24}>
+                    <Button
+                      className={s.cardContainer_buttonsButtonRemove}
+                      type="primary"
+                      icon={<HeartFilled />}
+                      onClick={() => removeItemFromFavorites(item.id)}
+                    >
+                      Remove favorite
+                    </Button>
+                  </Col>
+                  <Col md={12} sm={24} xs={24}>
+                    <Link
+                      to={`/${slug(item.title)}/${item.id}`}
+                      className={s.cardContainer_buttonsLink}
+                    >
                       <Button
-                        className={s.cardContainer_buttonsButtonRemove}
-                        type="primary"
-                        icon={<HeartFilled />}
-                        onClick={() => removeItemFromFavorites(item.id)}
+                        type="default"
+                        icon={<ArrowRightOutlined />}
+                        className={s.cardContainer_buttonsLinkButton}
                       >
-                        Remove favorite
+                        Read more
                       </Button>
-                    </Col>
-                    <Col md={12} sm={24} xs={24}>
-                      <Link
-                        to={`/${slug(item.title)}/${item.id}`}
-                        className={s.cardContainer_buttonsLink}
-                      >
-                        <Button
-                          type="default"
-                          icon={<ArrowRightOutlined />}
-                          className={s.cardContainer_buttonsLinkButton}
-                        >
-                          Read more
-                        </Button>
-                      </Link>
-                    </Col>
-                  </Row>
-                </Card>
-              </Col>
-            ))}
-        </Row>
-      )}
+                    </Link>
+                  </Col>
+                </Row>
+              </Card>
+            </Col>
+          ))}
+      </Row>
     </Layout>
   );
 }
