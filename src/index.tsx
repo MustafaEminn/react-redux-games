212b8d5e43a835ee57a07ed98536e52247a2b8ed
Mustafa Emin Bosnalı <mustafaeminbosnali@hotmail.com>
2021-11-08 14:32:02 +0300

    index.tsx; GameView and FavoritesView added.

--- src/index.tsx
+++ src/index.tsx
@@ -10,10 +10,18 @@ import "./declares";
 import "antd/dist/antd.css";
 import "./index.scss";
 import { ToastContainer } from "react-toastify";
+import FavoritesView from "./views/Favorites/Favorites";
+import GameView from "./views/Game/Game";
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <Switch>
+        <Route path="/:gameName/:gameId">
+          <GameView />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesView />
+        </Route>
         <Route path="/">
           <HomeView />
         </Route>
